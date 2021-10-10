996c24025e127007fb706fdd7d0e0e7d218068dc
Arnav <arnav130496@gmail.com>
2021-10-10 10:47:54 +0530

    repo init

---
+++ src/main/java/com/arnavsaraf/bookstagramdataloader/BookstagramDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.arnavsaraf.bookstagramdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstagramDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstagramDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arnavsaraf/bookstagramdataloader/BookstagramDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnavsaraf.bookstagramdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstagramDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
